254435cc73fa11c3f8a06547e330ccbed6de76a6
edvaldojr <edvaldoluizj@gmail.com>
2023-12-06 15:13:02 -0300

    Exercicios - Tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -1,4 +1,26 @@
 package academy.devdojo.maratonajava.introducao;
 
+/*
+Prática:
+
+Crie váriaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço> confirmo que recebi o salário de <salário>,
+ na data <data>
+ */
+
 public class Aula03TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Junior";
+        String endereco = "Rua campinas n° 45 bloco J0 ap 03";
+        double salario = 3000D;
+        String dataRecebimentoSalario = "06/12/2023";
+        String relatorio = "Eu, " +nome+ " morando no endereço " +endereco+ " confirmo que recebi o salário de " +salario+
+                " na data " +dataRecebimentoSalario;
+
+        System.out.println(relatorio);
+
+    }
 }
