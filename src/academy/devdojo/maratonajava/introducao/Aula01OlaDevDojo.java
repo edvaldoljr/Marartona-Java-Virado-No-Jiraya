b873bfba519b51318dc7ae542b3db3ac7995b253
edvaldojr <edvaldoluizj@gmail.com>
2023-12-06 13:54:03 -0300

    Curso Java virado no Jiraya

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * isso é um comentario <b>javadoc<b/>
+ * @Autor Edvaldo
+ */
+public class Aula01OlaDevDojo {
+    // Isso é um comentário de uma linha
+
+    /*
+    Isso é um comentário
+    de multiplas
+    linhas
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo! ");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        //Existem 8 tipos primitivos: int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFlout = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeira = true;
+        boolean falso = false;
+        char caractere = 87;
+
+        System.out.println("A idade é: " + idade + " anos");
+        System.out.println(caractere);
+    }
+}
