056849381d46a77b59dfa16b6a8cc06499b4baf1
edvaldojr <edvaldoluizj@gmail.com>
2023-12-07 19:44:14 -0300

    Aula05EstruturasCondicionais04

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -26,6 +26,7 @@ public class Aula05EstruturasCondicionais03 {
 
         //Ou até de outra maneira
         resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : " Ainda não tenho condições, mas vou ter!";
+
         System.out.println(resultado);
     }
 }
